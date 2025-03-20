9c41bf6d50eebbc9767879ace09dc6c846580269
yosh1 <yoppy1125yoppy@gmail.com>
2025-03-21 03:46:54 +0900

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Yoshihisa Kaino',
+  description: 'Yoshihisa Kaino',
+  generator: 'Yoshihisa Kaino',
 }
 
 export default function RootLayout({
